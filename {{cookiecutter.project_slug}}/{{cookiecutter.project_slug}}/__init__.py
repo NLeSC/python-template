6f22d94965e1282dd795d83cd074416aeb4ac9e1
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2018-07-04 11:17:32 +0200

    Add version to package

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 {% if cookiecutter.apidoc == 'yes' %}"""Documentation about {{ cookiecutter.project_name }}"""{% endif %}
+
+__version__ = '{{ cookiecutter.version }}'
+
 __author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
