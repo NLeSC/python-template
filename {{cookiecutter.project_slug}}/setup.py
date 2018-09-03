295a539cd0bf65ba4cebdee2a517bef4b010e691
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-09-03 14:06:53 +0200

    Added install_requires key to setup
    Fixes #21

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -27,6 +27,7 @@ setup(
     ],
     package_dir={'{{ cookiecutter.project_slug }}':
                  '{{ cookiecutter.project_slug }}'},
+    install_requires=[],
     include_package_data=True,
 {%- if cookiecutter.open_source_license in license_classifiers %}
     license="{{ cookiecutter.open_source_license }}",
