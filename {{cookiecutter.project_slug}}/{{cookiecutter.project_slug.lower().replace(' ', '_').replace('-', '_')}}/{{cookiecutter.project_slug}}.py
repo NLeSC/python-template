2508ab78c4a9c0dafce1d5ac14d1b354478e7d1d
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-07-17 16:22:11 +0200

    Remove version of file with possibly a wrong name
    The name may contain a dash (-) or a space.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug.lower().replace(' ', '_').replace('-', '_')}}/{{cookiecutter.project_slug}}.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-{%-if cookiecutter.apidoc == 'yes' %}"""Documentation about the {{ cookiecutter.project_name }} module."""{% endif %}
-
-
-# FIXME: put actual code here
-def example():
-    pass
