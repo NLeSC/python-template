2ca5e4291e3006eeed3f4587c8c5d9cc7feb25ce
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-07-24 09:25:31 +0200

    Re-add Python 3.5 as supported Python version
    Sphinx still works on Python 3.5, so re-added 3.5 as supported version
    in the project that is generated.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -47,6 +47,7 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
