ebe65a75dd9e87d466537e499a42f1c6a3a3e09f
Carlos Martinez <neocarlitos@gmail.com>
2020-05-20 23:17:47 +0200

    Add CITATION.cff file

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug.lower().replace(' ', '_').replace('-', '_')}}/__version__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug.lower().replace(' ', '_').replace('-', '_')}}/__version__.py
@@ -1,1 +1,9 @@
-__version__ = '{{ cookiecutter.version }}'
+import os
+import sys
+
+# To update the package version number, edit CITATION.cff
+citationfile = os.path.join(sys.exec_prefix, 'citation/{{ cookiecutter.project_slug.lower().replace(' ', '_').replace('-', '_')}}', 'CITATION.cff')
+with open(citationfile, 'r') as cff:
+    for line in cff:
+        if 'version:' in line:
+            __version__ = line.replace('version:', '').strip().strip('"')
