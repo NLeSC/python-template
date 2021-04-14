e12769a8f7f946aeca72cba6dce3ca5a95c7c1bf
Erik Tjong Kim Sang <erikt@xs4all.nl>
2021-04-14 12:58:53 +0200

    changed call of pytest (4)

--- tests/test_project.py
+++ tests/test_project.py
@@ -59,8 +59,7 @@ def test_running_tests(cookies):
     os.chdir(str(project.project))
 
     try:
-        sh.python(['setup.py', 'install'])
-        sh.python(['pytest'])
+        sh.pytest()
     except sh.ErrorReturnCode as e:
         pytest.fail(e)
     finally:
