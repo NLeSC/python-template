7bffc8716fb804c26b8e6bf72c334a6dd2a39ef1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-04-19 11:49:42 +0200

    Update {{cookiecutter.project_name}}/docs/make.bat
    Co-authored-by: Faruk D. <fdiblen@users.noreply.github.com>

--- {{cookiecutter.project_name}}/docs/make.bat
+++ {{cookiecutter.project_name}}/docs/make.bat
@@ -9,7 +9,7 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
-set SPHINXPROJ={{ cookiecutter.project_name }}
+set SPHINXPROJ={{ cookiecutter.package_name }}
 
 if "%1" == "" goto help
 
