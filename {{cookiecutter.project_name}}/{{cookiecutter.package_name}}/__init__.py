ef0c3a6abba3e75c796a6fa67464c8730cc09527
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-04-19 17:51:37 +0200

    template now compliant with isort configuration

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,7 +1,7 @@
 {% if cookiecutter.apidoc == "yes" %}"""Documentation about {{ cookiecutter.package_name }}"""{% endif %}
 import logging
-
 from .__version__ import __version__
+
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
