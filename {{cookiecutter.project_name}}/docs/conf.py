832f7f467fd124dd7483e7bddb9709dade5c95ba
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-04-19 21:24:41 +0200

    indent fix

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -133,7 +133,7 @@ html_context = {
     "css_files": [
         "_static/theme_overrides.css"  # override wide tables in RTD theme
     ]
- }
+}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
