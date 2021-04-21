152b7e29d47cfda88ea07ca679bf4e496a5e81e3
Faruk D <fdiblen@users.noreply.github.com>
2021-04-21 18:00:42 +0200

    Update test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -17,7 +17,7 @@ def run(command: str, dirpath: os.PathLike) -> subprocess.CompletedProcess:
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
                           cwd=dirpath,
-                          text=False)
+                          text=True)
 
 
 def test_pytest(cookies):
