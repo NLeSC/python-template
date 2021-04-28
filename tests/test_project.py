cba713a0bf8efae7cf2f3ab6a6cf0b86153b5bcb
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-04-28 14:38:31 +0200

    Point to actual exec

--- tests/test_project.py
+++ tests/test_project.py
@@ -23,7 +23,7 @@ def run(command: str, dirpath: os.PathLike) -> subprocess.CompletedProcess:
 
 def test_pytest(cookies):
     env_bin = 'env/Scripts/' if platform.startswith("win") else 'env/bin/'
-    pyexe = '/c/hostedtoolcache/windows/Python/3.9.4/x64/python3' if platform.startswith("win") else 'python3'
+    pyexe = '/c/hostedtoolcache/windows/Python/3.9.4/x64/python3.exe' if platform.startswith("win") else 'python3'
     result = cookies.bake()
     env_output = run(f'{pyexe} -m venv env', result.project)
     print(env_output.stdout)
