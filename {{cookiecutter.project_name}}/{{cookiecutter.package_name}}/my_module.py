d12b63dffb95e919ca70e52667bdd2f8b13f1ed2
Faruk D <f.diblen@esciencecenter.nl>
2021-04-29 17:19:23 +0200

    fix pep8 linting issue

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/my_module.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/my_module.py
@@ -4,6 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 # FIXME: put actual code here
 def example():
     logger.info("Providing information about the excecution of the function.")
