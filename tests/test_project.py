33046cd617cf8af74c2ff47cf85a1926a1e4457c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-04-29 11:24:51 +0200

    Add test to check subpackage is included in build

--- tests/test_project.py
+++ tests/test_project.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import shlex
 
+import pytest
+
 
 def test_project_folder(cookies):
     project = cookies.bake()
@@ -20,7 +22,8 @@ def run(command: str, dirpath: os.PathLike) -> subprocess.CompletedProcess:
                           encoding="utf-8")
 
 
-def test_pytest(cookies):
+@pytest.fixture
+def baked_withdevdeps(cookies):
     result = cookies.bake()
     env_output = run('python3 -m venv env', result.project)
     assert env_output.returncode == 0
@@ -28,9 +31,23 @@ def test_pytest(cookies):
     assert latest_pip_output.returncode == 0
     pip_output = run('env/bin/pip3 install --editable .[dev]', result.project)
     assert pip_output.returncode == 0
+    return result.project
 
-    pytest_output = run('env/bin/pytest', result.project)
+
+def test_pytest(baked_withdevdeps):
+    project_dir = baked_withdevdeps
+    pytest_output = run('env/bin/pytest', project_dir)
     assert pytest_output.returncode == 0
-    assert '== 3 passed in' in  pytest_output.stdout
-    assert (result.project / 'coverage.xml').exists()
-    assert (result.project / 'htmlcov/index.html').exists()
+    assert '== 3 passed in' in pytest_output.stdout
+    assert (project_dir / 'coverage.xml').exists()
+    assert (project_dir / 'htmlcov/index.html').exists()
+
+
+def test_subpackage(baked_withdevdeps):
+    project_dir = baked_withdevdeps
+    subpackage = (project_dir / 'my_python_package' / 'mysub')
+    subpackage.mkdir()
+    (subpackage / '__init__.py').write_text('FOO = "bar"', encoding="utf-8")
+    build_output = run('python3 setup.py build', project_dir)
+    assert build_output.returncode == 0
+    assert (project_dir / 'build' / 'lib' / 'my_python_package' / 'mysub' / '__init__.py').exists()
