fb05ef4536fcb6adb0db2e0991ae8e6dd57413d7
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2021-04-30 12:56:57 +0200

    Use full shpinx build command
    As Windows does not have make

--- tests/test_project.py
+++ tests/test_project.py
@@ -15,16 +15,15 @@ def test_project_folder(cookies):
     assert project.project.isdir()
 
 
-def run(args: Sequence[str], dirpath: os.PathLike, env_bin=None) -> subprocess.CompletedProcess:
-    env = os.environ.copy()
-    if env_bin:
-        env['PATH'] = os.getcwd() + env_bin + ':' + os.environ.get('PATH')
-    return subprocess.run(args=args,
+def run(args: Sequence[str], dirpath: os.PathLike) -> subprocess.CompletedProcess:
+    completed_process = subprocess.run(args=args,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
                           cwd=dirpath,
-                          env=env,
                           encoding="utf-8")
+    print(completed_process.stdout)
+    print(completed_process.stderr)
+    return completed_process
 
 
 @pytest.fixture
@@ -33,16 +32,16 @@ def baked_with_development_dependencies(cookies):
     env_output = run(['python3', '-m', 'venv', 'env'], result.project)
     assert env_output.returncode == 0
     env_bin = 'env/Scripts/' if platform.startswith("win") else 'env/bin/'
-    latest_pip_output = run(['pip3', 'install', '--upgrade', 'pip', 'setuptools'], result.project, env_bin)
+    latest_pip_output = run([f'{env_bin}pip3', 'install', '--upgrade', 'pip', 'setuptools'], result.project)
     assert latest_pip_output.returncode == 0
-    pip_output = run(['pip3', 'install', '--editable', '.[dev]'], result.project, env_bin)
+    pip_output = run([f'{env_bin}pip3', 'install', '--editable', '.[dev]'], result.project)
     assert pip_output.returncode == 0
     return result.project, env_bin
 
 
 def test_pytest(baked_with_development_dependencies):
     project_dir, env_bin = baked_with_development_dependencies
-    pytest_output = run(['pytest'], project_dir, env_bin)
+    pytest_output = run([f'{env_bin}pytest'], project_dir)
     assert pytest_output.returncode == 0
     assert '== 3 passed in' in pytest_output.stdout
     assert (project_dir / 'coverage.xml').exists()
@@ -59,7 +58,7 @@ def test_subpackage(baked_with_development_dependencies):
     subsubpackage.mkdir()
     (subsubpackage / '__init__.py').write_text('FOO = "bar"', encoding="utf-8")
 
-    build_output = run([f'python3', 'setup.py', 'build'], project_dir, env_bin)
+    build_output = run([f'{env_bin}python', 'setup.py', 'build'], project_dir)
     assert build_output.returncode == 0
     assert (project_dir / 'build' / 'lib' / 'my_python_package' / 'mysub' / '__init__.py').exists()
     assert (project_dir / 'build' / 'lib' / 'my_python_package' / 'mysub' / 'mysub2' / '__init__.py').exists()
@@ -68,7 +67,6 @@ def test_subpackage(baked_with_development_dependencies):
 def test_generate_api_docs(baked_with_development_dependencies):
     project_dir, env_bin = baked_with_development_dependencies
 
-    docs_dir = project_dir / 'docs'
-    build_output = run([f'make', 'html'], docs_dir, env_bin)
+    build_output = run([f'{env_bin}sphinx-build', '-b', 'html', 'docs', 'docs/_build/html'], project_dir)
     assert build_output.returncode == 0
-    assert (docs_dir / '_build' / 'html' / 'index.html').exists()
+    assert (project_dir / 'docs' / '_build' / 'html' / 'index.html').exists()
