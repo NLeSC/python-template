aed9f1c74960ca947dc1d5cc7a3ee8e08f01bb86
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-05-03 07:31:03 +0200

    bin_dir relative to pip

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from pathlib import Path
+from shutil import which
 from sys import platform, executable
 from typing import Sequence
 
@@ -33,16 +34,17 @@ def baked_with_development_dependencies(cookies):
     if platform.startswith("win") and os.environ.get('CI', False):
         # Creating virtualenv does not work on Windows CI,
         # falling back to using current Python
-        bin_dir = str(Path(executable).parent) + '\\'
+        pip = Path(which('pip3'))
+        bin_dir = str(pip.parent) + '\\'
         print(bin_dir)
         print(list(Path(bin_dir).iterdir()))
     else:
         env_output = run(['python3', '-m', 'venv', 'env'], result.project)
         assert env_output.returncode == 0
         bin_dir = 'env/Scripts/' if platform.startswith("win") else 'env/bin/'
-    latest_pip_output = run([f'{bin_dir}pip', 'install', '--upgrade', 'pip', 'setuptools'], result.project)
+    latest_pip_output = run([f'{bin_dir}pip3', 'install', '--upgrade', 'pip', 'setuptools'], result.project)
     assert latest_pip_output.returncode == 0
-    pip_output = run([f'{bin_dir}pip', 'install', '--editable', '.[dev]'], result.project)
+    pip_output = run([f'{bin_dir}pip3', 'install', '--editable', '.[dev]'], result.project)
     assert pip_output.returncode == 0
     return result.project, bin_dir
 
