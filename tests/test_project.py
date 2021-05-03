2222a36785338613ddfd5c0a2fbe19732f88d4da
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-05-03 07:08:35 +0200

    Check sphinx-build output

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from sys import platform
+from pathlib import Path
+from sys import platform, executable
 from typing import Sequence
 
 import pytest
@@ -17,10 +18,10 @@ def test_project_folder(cookies):
 
 def run(args: Sequence[str], dirpath: os.PathLike) -> subprocess.CompletedProcess:
     completed_process = subprocess.run(args=args,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          cwd=dirpath,
-                          encoding="utf-8")
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       cwd=dirpath,
+                                       encoding="utf-8")
     print(completed_process.stdout)
     print(completed_process.stderr)
     return completed_process
@@ -29,19 +30,24 @@ def run(args: Sequence[str], dirpath: os.PathLike) -> subprocess.CompletedProces
 @pytest.fixture
 def baked_with_development_dependencies(cookies):
     result = cookies.bake()
-    env_output = run(['python3', '-m', 'venv', 'env'], result.project)
-    assert env_output.returncode == 0
-    env_bin = 'env/Scripts/' if platform.startswith("win") else 'env/bin/'
-    latest_pip_output = run([f'{env_bin}pip3', 'install', '--upgrade', 'pip', 'setuptools'], result.project)
+    if platform.startswith("win") and os.environ.get('CI', False):
+        # Creating virtualenv does not work on Windows CI,
+        # falling back to using current Python
+        bin_dir = str(Path(executable).parent) + '\\'
+    else:
+        env_output = run(['python3', '-m', 'venv', 'env'], result.project)
+        assert env_output.returncode == 0
+        bin_dir = 'env/Scripts/' if platform.startswith("win") else 'env/bin/'
+    latest_pip_output = run([f'{bin_dir}pip3', 'install', '--upgrade', 'pip', 'setuptools'], result.project)
     assert latest_pip_output.returncode == 0
-    pip_output = run([f'{env_bin}pip3', 'install', '--editable', '.[dev]'], result.project)
+    pip_output = run([f'{bin_dir}pip3', 'install', '--editable', '.[dev]'], result.project)
     assert pip_output.returncode == 0
-    return result.project, env_bin
+    return result.project, bin_dir
 
 
 def test_pytest(baked_with_development_dependencies):
-    project_dir, env_bin = baked_with_development_dependencies
-    pytest_output = run([f'{env_bin}pytest'], project_dir)
+    project_dir, bin_dir = baked_with_development_dependencies
+    pytest_output = run([f'{bin_dir}pytest'], project_dir)
     assert pytest_output.returncode == 0
     assert '== 3 passed in' in pytest_output.stdout
     assert (project_dir / 'coverage.xml').exists()
@@ -49,7 +55,7 @@ def test_pytest(baked_with_development_dependencies):
 
 
 def test_subpackage(baked_with_development_dependencies):
-    project_dir, env_bin = baked_with_development_dependencies
+    project_dir, bin_dir = baked_with_development_dependencies
     subpackage = (project_dir / 'my_python_package' / 'mysub')
     subpackage.mkdir()
     (subpackage / '__init__.py').write_text('FOO = "bar"', encoding="utf-8")
@@ -58,15 +64,16 @@ def test_subpackage(baked_with_development_dependencies):
     subsubpackage.mkdir()
     (subsubpackage / '__init__.py').write_text('FOO = "bar"', encoding="utf-8")
 
-    build_output = run([f'{env_bin}python', 'setup.py', 'build'], project_dir)
+    build_output = run([f'{bin_dir}python', 'setup.py', 'build'], project_dir)
     assert build_output.returncode == 0
     assert (project_dir / 'build' / 'lib' / 'my_python_package' / 'mysub' / '__init__.py').exists()
     assert (project_dir / 'build' / 'lib' / 'my_python_package' / 'mysub' / 'mysub2' / '__init__.py').exists()
 
 
 def test_generate_api_docs(baked_with_development_dependencies):
-    project_dir, env_bin = baked_with_development_dependencies
+    project_dir, bin_dir = baked_with_development_dependencies
 
-    build_output = run([f'{env_bin}sphinx-build', '-b', 'html', 'docs', 'docs/_build/html'], project_dir)
+    build_output = run([f'{bin_dir}sphinx-build', '-b', 'html', 'docs', 'docs/_build/html'], project_dir)
     assert build_output.returncode == 0
+    assert 'build succeeded' in build_output.stdout
     assert (project_dir / 'docs' / '_build' / 'html' / 'index.html').exists()
