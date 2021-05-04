598ad36898cdaa6570b7d2c67225d13dddf8e22a
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-05-04 07:27:16 +0200

    Remove test examples
    Goto pytest docs for examples.
    Only test own code

--- {{cookiecutter.directory_name}}/tests/test_my_module.py
+++ {{cookiecutter.directory_name}}/tests/test_my_module.py
@@ -1,30 +1,7 @@
-#!/usr/bin/env python
-
 """Tests for the {{ cookiecutter.package_name }}.my_module module.
 """
-import pytest
 from {{ cookiecutter.package_name }} import my_module
 
 
 def test_hello():
     assert my_module.hello('nlesc') == 'Hello nlesc!'
-
-
-def test_something():
-    assert True
-
-
-def test_with_error():
-    with pytest.raises(ValueError):
-        # Do something that raises a ValueError
-        raise(ValueError)
-
-
-# Fixture example
-@pytest.fixture
-def an_object():
-    return {}
-
-
-def test_my_module(an_object):
-    assert an_object == {}
