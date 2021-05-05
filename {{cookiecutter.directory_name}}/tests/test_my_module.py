b6452e0c2972641730e517a715d56cd3f7d2c559
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-05-05 20:56:55 +0200

    Only use example function in tests

--- {{cookiecutter.directory_name}}/tests/test_my_module.py
+++ {{cookiecutter.directory_name}}/tests/test_my_module.py
@@ -1,7 +1,24 @@
 """Tests for the {{ cookiecutter.package_name }}.my_module module.
 """
+import pytest
+
 from {{ cookiecutter.package_name }} import my_module
 
 
 def test_hello():
     assert my_module.hello('nlesc') == 'Hello nlesc!'
+
+
+def test_hello_with_error():
+    with pytest.raises(ValueError) as excinfo:
+        my_module.hello('nobody')
+        assert str(excinfo.value) == 'Can not say hello to nobody'
+
+
+@pytest.fixture
+def some_name():
+    return 'Jane Smith'
+
+
+def test_hello_with_fixture(some_name):
+    assert my_module.hello(some_name) == 'Hello Jane Smith!'
--- {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/my_module.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/my_module.py
@@ -5,12 +5,18 @@
 def hello(name):
     """Say hello
 
-    Example docstring using Google docstring format.
+    Example docstring using Google docstring style.
 
     Args:
         name (str): Name to say hello to
 
     Returns:
         str: Hello message
+
+    Raises:
+        ValueError: If `name` is equal to `nobody`
+
     """
+    if name == 'nobody':
+        raise ValueError('Can not say hello to nobody')
     return f'Hello {name}!'
