e3d127554cd2e0d40a21f9b5478e8c8a7d80ed49
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-09-06 16:40:06 +0200

    bring template code and ruff config in sync (Refs #336)

--- {{cookiecutter.directory_name}}/setup.py
+++ {{cookiecutter.directory_name}}/setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import os
 from setuptools import setup
-
 
 # see setup.cfg
 setup()
--- {{cookiecutter.directory_name}}/tests/test_my_module.py
+++ {{cookiecutter.directory_name}}/tests/test_my_module.py
@@ -1,15 +1,15 @@
-"""Tests for the {{ cookiecutter.package_name }}.my_module module.
-"""
+"""Tests for the {{ cookiecutter.package_name }}.my_module module."""
 import pytest
-
 from {{ cookiecutter.package_name }}.my_module import hello
 
 
 def test_hello():
+    """Example using assert."""
     assert hello('nlesc') == 'Hello nlesc!'
 
 
 def test_hello_with_error():
+    """Example of testing for raised errors."""
     with pytest.raises(ValueError) as excinfo:
         hello('nobody')
     assert 'Can not say hello to nobody' in str(excinfo.value)
@@ -17,8 +17,10 @@ def test_hello_with_error():
 
 @pytest.fixture
 def some_name():
+    """Example fixture."""
     return 'Jane Smith'
 
 
 def test_hello_with_fixture(some_name):
+    """Example using a fixture."""
     assert hello(some_name) == 'Hello Jane Smith!'
